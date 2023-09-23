a6fcebd6145427552b2502c52d43c38d314410ed
mehta <mehta@LAPTOP-B1LKLJTG>
2023-09-23 12:40:21 +0530

    First commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,31 @@
 package com.driver;
 
 public class Main {
+	
+	public static void main(String[] args) {
+		Product p = new Product();
+		
+		p.product(3, 4);
+		
+		p.product(3, 4, 5);
+		
+		p.product(4.0, 5.0);
+	}
 
+}
+
+class Product {
+	
+	public int product(int x, int y) {
+		return x*y;
+	}
+	
+	public int product(int x, int y, int z) {
+		return x*y*z;
+	}
+	
+	public double product(double x, double y) {
+		return x*y;
+	}
+	
 }
